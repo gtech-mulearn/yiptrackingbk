25e4b58194dcf55e8ae417beb3c07010e9e5c857
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-29 19:18:15 +0530

    minor changes

--- api/auth/serializers.py
+++ api/auth/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     user_id = serializers.CharField(source='id', read_only=True)
     password = serializers.CharField(write_only=True)
-    
+
     def create(self, validated_data):
         password = validated_data.pop("password")
         hashed_password = make_password(password)
@@ -42,15 +42,15 @@ class UserSerializer(serializers.ModelSerializer):
         ]
 
 
-class UserUpdateSerializer(serializers.ModelSerializer):
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.password = validated_data.get('password', instance.password)
-        instance.save()
-        return instance
+# class UserUpdateSerializer(serializers.ModelSerializer):
+#     def update(self, instance, validated_data):
+#         instance.first_name = validated_data.get('first_name', instance.first_name)
+#         instance.last_name = validated_data.get('last_name', instance.last_name)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.password = validated_data.get('password', instance.password)
+#         instance.save()
+#         return instance
 
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'password']
+#     class Meta:
+#         model = User
+#         fields = ['first_name', 'last_name', 'email', 'password']
