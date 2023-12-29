0f908945a43d66faec1b330b52f2b4c0932bd272
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-29 19:16:33 +0530

    minor optimizations

--- api/auth/serializers.py
+++ api/auth/serializers.py
@@ -4,10 +4,14 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    user_id = serializers.CharField(source='id', read_only=True)
+    password = serializers.CharField(write_only=True)
+    
     def create(self, validated_data):
         password = validated_data.pop("password")
         hashed_password = make_password(password)
         validated_data['password'] = hashed_password
+        validated_data['updated_by'] = validated_data['created_by']
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
@@ -20,7 +24,22 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password']
+        fields = [
+            'user_id',
+            'first_name',
+            'last_name',
+            'role',
+            'email',
+            'mobile',
+            'password',
+            'gender',
+            'dob',
+            'district_id',
+            'org_id',
+            'updated_at',
+            'created_at',
+            'created_by'
+        ]
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
--- db/models.py
+++ db/models.py
@@ -13,12 +13,12 @@ class User(models.Model):
     mobile = models.CharField(max_length=15,null=True,blank=False)
     gender = models.CharField(max_length=10,null=True,blank=False)
     dob = models.DateField(null=True,blank=False)
-    district_id = models.ForeignKey('District',on_delete=models.CASCADE,null=True,blank=False)
-    org_id = models.ForeignKey('Organization',on_delete=models.CASCADE,null=True,blank=False)
-    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    district_id = models.ForeignKey('District',on_delete=models.CASCADE,null=True,blank=False,related_name='user_district_id',db_column='district_id')
+    org_id = models.ForeignKey('Organization',on_delete=models.CASCADE,null=True,blank=False, related_name='user_org_id', db_column='org_id')
+    updated_by = models.CharField(max_length=36,null=False,blank=False)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    created_by = models.CharField(max_length=36,null=False,blank=False)
 
     class Meta:
         managed = False
@@ -27,11 +27,11 @@ class User(models.Model):
 class District(models.Model):
     id = models.CharField(primary_key=True,max_length=36,unique=True,null=False,blank=False,default=uuid.uuid4)
     name = models.CharField(max_length=75,null=False,blank=False)
-    zone_id = models.ForeignKey('Zone',on_delete=models.CASCADE,null=False,blank=False)
-    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    zone_id = models.ForeignKey('Zone',on_delete=models.CASCADE,null=False,blank=False, related_name='district_zone_id', db_column='zone_id')
+    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='district_updated_by', db_column='updated_by')
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='district_created_by', db_column='created_by')
 
     class Meta:
         managed = False
@@ -40,10 +40,10 @@ class District(models.Model):
 class Zone(models.Model):
     id = models.CharField(primary_key=True,max_length=36,unique=True,null=False,blank=False,default=uuid.uuid4)
     name = models.CharField(max_length=75,null=False,blank=False)
-    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='zone_updated_by', db_column='updated_by')
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='zone_created_by', db_column='created_by')
 
     class Meta:
         managed = False
@@ -54,11 +54,11 @@ class Organization(models.Model):
     title = models.CharField(max_length=100,null=False,blank=False)
     code = models.CharField(max_length=12,null=False,blank=False)
     org_type = models.CharField(max_length=25,null=False,blank=False)
-    district_id = models.ForeignKey('District',on_delete=models.CASCADE,null=True,blank=False)
-    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    district_id = models.ForeignKey('District',on_delete=models.CASCADE,null=True,blank=False, related_name='org_district_id', db_column='district_id')
+    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='org_updated_by', db_column='updated_by')
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='org_created_by', db_column='created_by')
 
     class Meta:
         managed = False
@@ -66,8 +66,8 @@ class Organization(models.Model):
 
 class UserOrgLink(models.Model):
     id = models.CharField(primary_key=True,max_length=36,unique=True,null=False,blank=False,default=uuid.uuid4)
-    user_id = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
-    org_id = models.ForeignKey('Organization',on_delete=models.CASCADE,null=False,blank=False)
+    user_id = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='user_org_link_user_id', db_column='user_id')
+    org_id = models.ForeignKey('Organization',on_delete=models.CASCADE,null=False,blank=False, related_name='user_org_link_org_id', db_column='org_id')
     visited = models.BooleanField(null=False,blank=False,default=False)
     pta = models.CharField(max_length=255,null=True,blank=False)
     alumini = models.CharField(max_length=255,null=True,blank=False)
@@ -75,9 +75,9 @@ class UserOrgLink(models.Model):
     whatsapp = models.CharField(max_length=255,null=True,blank=False)
     participants = models.BigIntegerField(null=False,blank=False,default=0)
     created_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    created_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='user_org_link_created_by', db_column='created_by')
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False)
+    updated_by = models.ForeignKey('User',on_delete=models.CASCADE,null=False,blank=False, related_name='user_org_link_updated_by', db_column='updated_by')
 
     class Meta:
         managed = False
