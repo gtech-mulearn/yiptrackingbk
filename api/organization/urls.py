91192b17b5b460c62d7cce7769508f0becf79794
Aashish <8xdeadshot@gmail.com>
2023-12-31 17:39:43 +0530

    minor optim and spacing

--- api/organization/urls.py
+++ api/organization/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import OrganizationAPI, AssignOrganizationAPI, OrgVisitedAPI
+
 urlpatterns = [
     path('', OrganizationAPI.as_view()),
     path('create/', OrganizationAPI.as_view()),
-    path('assign/',AssignOrganizationAPI.as_view()),
-    path('update-status/',OrgVisitedAPI.as_view()),
+    path('assign/', AssignOrganizationAPI.as_view()),
+    path('update-status/', OrgVisitedAPI.as_view()),
 ]
