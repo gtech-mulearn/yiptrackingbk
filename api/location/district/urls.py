171623a4aa2fc933c6d0998b84a9a16b4e69b741
Aswanth Vc <aswanth.abam@gmail.com>
2024-01-04 21:38:31 +0530

    feat(district) : District Summary API

--- api/location/district/urls.py
+++ api/location/district/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DistrictAPI
+from .views import DistrictAPI, DistrictSummaryAPI
 
 urlpatterns = [
     path('', DistrictAPI.as_view()),
     path('create/', DistrictAPI.as_view()),
+    path('summary/', DistrictSummaryAPI.as_view()),
 ]
