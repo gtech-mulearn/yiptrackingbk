fcfb4d16484572946db4856054812f1d47ed1718
Aswanth Vc <aswanth.abam@gmail.com>
2024-01-14 12:45:08 +0530

    feat(ideaview) : idea view endpoints changed

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('auth/', include('api.auth.urls')),
     path('location/', include('api.location.urls')),
     path('organization/', include('api.organization.urls')),
+    path('ideaview/', include('api.ideaview.urls')),
     path("__debug__/", include(debug_toolbar.urls)),
 ]
